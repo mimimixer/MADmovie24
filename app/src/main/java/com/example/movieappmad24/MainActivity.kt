e9a5725e06e0227016682501a7bc79ca5e2d9ec0
mimimixer <113891038+mimimixer@users.noreply.github.com>
2024-03-10 21:47:39 +0100

    alles da, funzt und ist schön geordnet - gute nacht :)

--- app/src/main/java/com/example/movieappmad24/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/MainActivity.kt
@@ -1,39 +1,27 @@
-package com.example.movieappmad24.composables
+package com.example.movieappmad24
 
-import android.icu.text.CaseMap.Title
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.Badge
 import androidx.compose.material.BadgedBox
-import androidx.compose.material.BottomAppBar
-import androidx.compose.material.IconButton
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.Favorite
 import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.KeyboardArrowDown
 import androidx.compose.material.icons.outlined.KeyboardArrowUp
-import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -44,46 +32,36 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.saveable.rememberSaveableStateHolder
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.ColorFilter
-import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.graphicsLayer
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
-import com.example.movieappmad24.R
 import com.example.movieappmad24.models.Movie
+import com.example.movieappmad24.models.bottomIcons
 import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
-import com.example.movieappmad24.ui.theme.Pink40
 import com.example.movieappmad24.ui.theme.Pink80
 import com.example.movieappmad24.ui.theme.Purple40
-import com.example.movieappmad24.ui.theme.Purple80
 
 
 class MainActivity : ComponentActivity() {
-    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -94,17 +72,16 @@ class MainActivity : ComponentActivity() {
                     color = MaterialTheme.colorScheme.background
                 ) {
                     Scaffold(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(),
                         topBar = {
                             MovieTopAppBar()
                         },
                         bottomBar = {
                             MovieBottomNavigationBar()
                         }
-                    ) { values ->
-                        MovieList(movies = allMovieNames, modifier = Modifier.padding(values))
+                    ) { values -> // 10.3.24 idea of a box from von https://stackoverflow.com/questions/67080426/compose-appbar-overlaps-activity-content
+                        Box (modifier = Modifier.padding(values)){
+                            MovieList(movies = getMovies(), 1)
+                        }
 
                     }
                 }
@@ -113,16 +90,17 @@ class MainActivity : ComponentActivity() {
     }
 }
 @Composable
-fun MovieList (movies: List<Movie>, modifier: Modifier){
+fun MovieList (movies: List<Movie>, number: Int){
     LazyColumn(modifier = Modifier
         .fillMaxSize()
     ){
         items(movies){movie ->
-            MovieRow(movie)
+            MovieCard(movie, number)
         }
     }
 }
 
+//creates a simple TopAppBar with centered title
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MovieTopAppBar() {
@@ -134,32 +112,15 @@ fun MovieTopAppBar() {
     )
 }
 
-data class BottomNavigationItem(
-    val title: String,
-    val selectedIcon: ImageVector,
-    val unselectedIcon: ImageVector
-)
-
-val bottomIcons = listOf(
-    BottomNavigationItem (
-        title = "Home",
-        selectedIcon = Icons.Filled.Home,
-        unselectedIcon = Icons.Default.Home
-    ),
-    BottomNavigationItem(
-        title = "Watchlist",
-        selectedIcon = Icons.Filled.Star,
-        unselectedIcon = Icons.Filled.Star
-    )
-)
-
-
+//creates a bottomBar with navigational buttons from bottomList
 @Composable
 fun MovieBottomNavigationBar() {
     var selectedItemIndex by rememberSaveable {
-        mutableStateOf(0)
+        mutableIntStateOf(0)
     }
     NavigationBar (modifier = Modifier
+        //10-3-24 finally found how to round corners
+        //https://stackoverflow.com/questions/72270597/bottom-nav-bar-with-curved-edge-and-shadow-jetpack-compose
         .graphicsLayer {
             clip = true
             shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp)
@@ -190,13 +151,12 @@ fun MovieBottomNavigationBar() {
     }
 }
 
-
 @Composable
-fun MovieRow(movie: Movie){
+fun MovieCard(movie: Movie, number: Int){
     var clickedArrow by remember {
         mutableStateOf(false)
     }
-    var heartyToClick by remember {
+    val heartyToClick by remember {
         mutableStateOf(true)
     }
     Card (
@@ -206,42 +166,18 @@ fun MovieRow(movie: Movie){
         shape = MaterialTheme.shapes.large,
         elevation = CardDefaults.cardElevation(10.dp)
     ) {
-    Column() {
+    Column{
         Box(modifier = Modifier
             .fillMaxWidth()
             .height(150.dp)
             ) {
-            AsyncImage(
-                // model = movie.images.random(),
-                ImageRequest.Builder(LocalContext.current)
-                    .data(movie.images[1])
-                    .crossfade(true)
-                    .build(),
-                placeholder = painterResource(id = R.drawable.movie_image),
-                contentScale = ContentScale.Crop,
-                contentDescription = "movie_image"
-            )
+            GetMoviePoster(movie = movie, number)
             Box(modifier = Modifier
                 .padding(10.dp)
                 .fillMaxWidth(),
                 contentAlignment = Alignment.TopEnd,
                 ) {
-                    if (heartyToClick) {
-                        Icon(
-                            modifier = Modifier
-                                .clickable { heartyToClick = !heartyToClick },
-                            imageVector = Icons.Outlined.FavoriteBorder,
-                            contentDescription = "little empty heart"
-                        )
-                    } else {
-                        Icon(
-                            modifier = Modifier
-                                .clickable { heartyToClick = !heartyToClick },
-                            imageVector = Icons.Outlined.Favorite,
-                            tint = Color.Red,
-                            contentDescription = "little red heart",
-                        )
-                    }
+                LikeMoviesHeart(heartyToClick)
             }
         }
         Row(
@@ -259,33 +195,73 @@ fun MovieRow(movie: Movie){
             )
         }
     }
-        AnimatedVisibility (clickedArrow) {
-            Column (modifier = Modifier
-                .padding(15.dp)
-            ) {
-                Text(text = "Director: ${movie.director}", color = Color.Black)
-                Text(text = "Released: ${movie.year}", color = Color.Black)
-                Text(text = "Genre: ${movie.genre}", color = Color.Black)
-                Text(text = "Actors: ${movie.actors}", color = Color.Black)
-                Text(text = "Rating: ${movie.rating}", color = Color.Black)
-                Divider()
-                Text(text = "Plot: ${movie.plot}", color = Color.Black)
-            }
+            GetMovieInfos(movie = movie, clickedArrow = clickedArrow)
+    }
+}
 
+@Composable
+fun LikeMoviesHeart (hearty: Boolean){
+    var heartyToClick by remember {
+        mutableStateOf(hearty)
+    }
+    if (heartyToClick) {
+        Icon(
+            modifier = Modifier
+                .clickable { heartyToClick = !heartyToClick },
+            imageVector = Icons.Outlined.FavoriteBorder,
+            contentDescription = "little empty heart"
+        )
+    } else {
+        Icon(
+            modifier = Modifier
+                .clickable { heartyToClick = !heartyToClick },
+            imageVector = Icons.Outlined.Favorite,
+            tint = Color.Red,
+            contentDescription = "little red heart",
+        )
+    }
+}
+
+@Composable
+fun GetMovieInfos (movie:Movie, clickedArrow: Boolean){
+    AnimatedVisibility (clickedArrow) {
+        Column (modifier = Modifier
+            .padding(15.dp)
+        ) {
+            Text(text = "Director: ${movie.director}")
+            Text(text = "Released: ${movie.year}")
+            Text(text = "Genre: ${movie.genre}")
+            Text(text = "Actors: ${movie.actors}")
+            Text(text = "Rating: ${movie.rating}")
+            Divider()
+            Text(text = "Plot: ${movie.plot}")
         }
     }
 }
 
+@Composable
+fun GetMoviePoster (movie: Movie, number: Int){
+    var poster = movie.images[1]
+    if (number != 1){
+        poster =movie.images.random()
+    }
+    AsyncImage(
+        ImageRequest.Builder(LocalContext.current)
+            .data(poster)
+            .crossfade(true)
+            .build(),
+        //for if you want only an image from a specific index:
+        // model =  movie.images[1],
+        placeholder = painterResource(id = R.drawable.movie_image),
+        contentScale = ContentScale.Crop,
+        contentDescription = "movie_image"
+    )
+}
 
 @Preview
 @Composable
 fun DefaultPreview(){
     MovieAppMAD24Theme {
-        MovieList(movies = allMovieNames, modifier = Modifier)
+        MovieList(movies = getMovies(),2)
     }
 }
-
-
-var allMovieNames = getMovies()
-
-    //listOf("Avatar",  "Blow-Up", "Casablanca", "Dune", "eXistenZ", "Freaks" )
---
+++ app/src/main/java/com/example/movieappmad24/models/BottomNavigationItem.kt
@@ -0,0 +1,28 @@
+package com.example.movieappmad24.models
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Home
+import androidx.compose.material.icons.outlined.Star
+import androidx.compose.ui.graphics.vector.ImageVector
+
+data class BottomNavigationItem(
+    val title: String,
+    val selectedIcon: ImageVector,
+    val unselectedIcon: ImageVector
+)
+
+val bottomIcons = listOf(
+    BottomNavigationItem(
+        title = "Home",
+        selectedIcon = Icons.Filled.Home,
+        unselectedIcon = Icons.Outlined.Home
+    ),
+    BottomNavigationItem(
+        title = "Watchlist",
+        selectedIcon = Icons.Filled.Star,
+        unselectedIcon = Icons.Outlined.Star
+
+    )
+)
