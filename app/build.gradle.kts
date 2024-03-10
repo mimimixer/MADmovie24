47e6112b5d36a6b09c230e8cf575666f74d68522
mimimixer <113891038+mimimixer@users.noreply.github.com>
2024-03-10 19:02:06 +0100

    little<3 red + top+bottom bars still ugly but more like the requirements

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -64,7 +64,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui:1.0.0")
-    implementation("androidx.compose.material:material:1.0.0")
+    implementation("androidx.compose.material:material:1.6.3")
     implementation("io.coil-kt:coil-compose:2.6.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
--- app/src/main/java/com/example/movieappmad24/composables/MainActivity.kt
+++ app/src/main/java/com/example/movieappmad24/composables/MainActivity.kt
@@ -1,20 +1,19 @@
-package com.example.movieappmad24
+package com.example.movieappmad24.composables
 
-import android.graphics.Paint.Align
-import android.media.ImageReader
+import android.icu.text.CaseMap.Title
 import android.os.Bundle
-import android.widget.GridLayout
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.animation.AnimatedVisibility
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -22,21 +21,27 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.BottomAppBar
 import androidx.compose.material.IconButton
-import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.outlined.Favorite
+import androidx.compose.material.icons.outlined.FavoriteBorder
 import androidx.compose.material.icons.outlined.KeyboardArrowDown
 import androidx.compose.material.icons.outlined.KeyboardArrowUp
+import androidx.compose.material3.BottomAppBarDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Shapes
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
@@ -49,18 +54,27 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
+import com.example.movieappmad24.R
 import com.example.movieappmad24.models.Movie
 import com.example.movieappmad24.models.getMovies
 import com.example.movieappmad24.ui.theme.MovieAppMAD24Theme
+import com.example.movieappmad24.ui.theme.Pink40
+import com.example.movieappmad24.ui.theme.Pink80
+import com.example.movieappmad24.ui.theme.Purple40
+import com.example.movieappmad24.ui.theme.Purple80
+
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3Api::class)
@@ -75,7 +89,8 @@ class MainActivity : ComponentActivity() {
                 ) {
                     Scaffold(
                         modifier = Modifier
-                            .fillMaxSize(),
+                            .fillMaxSize()
+                            .padding(),
                         topBar = {
                             MovieTopAppBar()
                         },
@@ -107,87 +122,137 @@ fun MovieList (movies: List<Movie>, modifier: Modifier){
 fun MovieTopAppBar() {
     CenterAlignedTopAppBar(
         title = {
-            Text("Movie App")
-        }
+            Text("Movie App", color = Purple40)
+        },
+        colors = TopAppBarDefaults.topAppBarColors(containerColor = Pink80)
     )
 }
+
+
 @Composable
 fun MovieBottomAppBar() {
-
             BottomAppBar(modifier = Modifier
-                .padding(0.dp)
-                .height(100.dp)) {
+                .height(70.dp)
+                .graphicsLayer {
+                    clip = true
+                    shape = RoundedCornerShape(bottomStart = 20.dp, bottomEnd = 20.dp)
+                    shadowElevation = 2.2f
+                },
+                backgroundColor = Pink80,
+            ) {
                 Row(
-                    Modifier
+                    modifier = Modifier
                         .fillMaxWidth(),
-                    horizontalArrangement = Arrangement.SpaceBetween
+                    horizontalArrangement = Arrangement.SpaceAround
                 ) {
-                    Column(modifier = Modifier
-                        .align(Alignment.CenterVertically)
-                        .clip(CircleShape)) {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(imageVector = Icons.Default.Home, contentDescription = "Home")
+          //              IconButton(modifier = Modifier.clickable {  },
+            //                onClick = { /*TODO*/ }
+              //          ) {
+                            Column(
+                                verticalArrangement = Arrangement.SpaceAround,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                            Icon(
+                                imageVector = Icons.Default.Home,
+                                contentDescription = "Home",
+                                tint = Color.Black
+                            )
+                            Text(text = "Home", color = Color.Black)
                         }
-                        Text(text = "Home", color = Color.Black)
-                    }
-                    Column {
-                        IconButton(onClick = { /*TODO*/ }) {
-                            Icon(imageVector = Icons.Default.Star, contentDescription = "Watchlist")
-                        }
-                        Text(text = "Watchlist", color = Color.Black)
-                    }
+             //       }
 
+              //          IconButton(modifier = Modifier.clickable {  },
+                //            onClick = { /*TODO*/ }
+                  //      ) {
+                            Column(
+                                verticalArrangement = Arrangement.SpaceAround,
+                                horizontalAlignment = Alignment.CenterHorizontally
+                            ) {
+                            Icon(
+                                imageVector = Icons.Default.Star,
+                                contentDescription = "Watchlist",
+                                tint = Color.Black
+                            )
+                            Text(text = "Watchlist", color = Color.Black)
+                        }
+                  //  }
                 }
             }
-
 }
 
 
 @Composable
 fun MovieRow(movie: Movie){
-    var clickedArrow by remember { mutableStateOf(false) }
-    Card {
-    Column(modifier = Modifier
-        .padding(Dp(12F))) {
+    var clickedArrow by remember {
+        mutableStateOf(false)
+    }
+    var heartyToClick by remember {
+        mutableStateOf(true)
+    }
+    Card (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(3.dp),
+        shape = MaterialTheme.shapes.large,
+        elevation = CardDefaults.cardElevation(10.dp)
+    ) {
+    Column() {
         Box(modifier = Modifier
-            .padding(0.dp)
-            .height(150.dp)) {
+            .fillMaxWidth()
+            .height(150.dp)
+            ) {
             AsyncImage(
-                model = movie.images.random(),
-                //ImageRequest.Builder(LocalContext.current)
-                  //  .data({movie.images.random()})
-                    //.crossfade(true)
-                    //.build(),
+                // model = movie.images.random(),
+                ImageRequest.Builder(LocalContext.current)
+                    .data(movie.images[1])
+                    .crossfade(true)
+                    .build(),
                 placeholder = painterResource(id = R.drawable.movie_image),
                 contentScale = ContentScale.Crop,
                 contentDescription = "movie_image"
             )
-            Box(modifier = Modifier.fillMaxWidth(),
-                contentAlignment = Alignment.TopEnd) {
-                Icon(
-                    painter = painterResource(id = R.drawable.outline_favorite_border_24),
-                    contentDescription = "little_heart"
-                )
+            Box(modifier = Modifier
+                .padding(10.dp)
+                .fillMaxWidth(),
+                contentAlignment = Alignment.TopEnd,
+                ) {
+                    if (heartyToClick) {
+                        Icon(
+                            modifier = Modifier
+                                .clickable { heartyToClick = !heartyToClick },
+                            imageVector = Icons.Outlined.FavoriteBorder,
+                            contentDescription = "little empty heart"
+                        )
+                    } else {
+                        Icon(
+                            modifier = Modifier
+                                .clickable { heartyToClick = !heartyToClick },
+                            imageVector = Icons.Outlined.Favorite,
+                            tint = Color.Red,
+                            contentDescription = "little red heart",
+                        )
+                    }
             }
         }
         Row(
             Modifier
                 .fillMaxWidth()
-                .background(color = Color.White),
+                .padding(10.dp),
             horizontalArrangement = Arrangement.SpaceBetween
         ) {
-            Text(text = movie.title, color = Color.Black)
+            Text(text = movie.title)
             Icon(modifier = Modifier.clickable { clickedArrow = !clickedArrow },
                 imageVector = if (clickedArrow) Icons.Outlined.KeyboardArrowDown
                 else Icons.Outlined.KeyboardArrowUp,
                // imageVector = Icons.Outlined.KeyboardArrowUp,
                 contentDescription = "little arrow toggle button",
-                tint = Color.Black
             )
         }
     }
         AnimatedVisibility (clickedArrow) {
-            Column {
+            Column (modifier = Modifier
+                .padding(15.dp)
+            ) {
                 Text(text = "Director: ${movie.director}", color = Color.Black)
                 Text(text = "Released: ${movie.year}", color = Color.Black)
                 Text(text = "Genre: ${movie.genre}", color = Color.Black)
