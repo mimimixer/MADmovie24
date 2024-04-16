71ee5e476c5e203bc8542e8b45be1ee0326f6a8e
mimimixer <113891038+mimimixer@users.noreply.github.com>
2024-04-16 14:29:01 +0200

    ex4: video fangt imme rnoch neu an wenn ich drehe grrrr

--- app/src/main/java/com/example/movieappmad24/composables/PlayerTrailer.kt
+++ app/src/main/java/com/example/movieappmad24/composables/PlayerTrailer.kt
@@ -4,27 +4,35 @@ import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableLongStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LifecycleEventObserver
 import androidx.media3.common.MediaItem
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.ui.PlayerView
 import com.example.movieappmad24.R
+import kotlinx.coroutines.delay
 
 @Composable
 fun PlayerTrailer (movieName: String){
 
-    var lifecycle by remember {
+    var lifecycle by rememberSaveable {
         mutableStateOf(Lifecycle.Event.ON_CREATE)
     }
+
+    var playbackPosition: Long = 0
+
     val context = LocalContext.current
 
     var trailerLocation = context.resources.getIdentifier(movieName, "raw", context.packageName)
@@ -34,10 +42,13 @@ fun PlayerTrailer (movieName: String){
 
     val movieTrailer = MediaItem.fromUri(
             //"https://file-examples.com/storage/fe793dd9be65a9b389251ea/2017/04/file_example_MP4_480_1_5MG.mp4"
-        "android.resource://${context.packageName}/${trailerLocation}"
+        //"android.resource://${context.packageName}/${trailerLocation}"
+        "android.resource://${context.packageName}/${R.raw.mon_film}"
+
     )
     val exoPlayer = remember {
         ExoPlayer.Builder(context).build().apply {
+            println("playing again")
             setMediaItem(movieTrailer)
             prepare()
             playWhenReady = true
@@ -48,6 +59,7 @@ fun PlayerTrailer (movieName: String){
 
     DisposableEffect(key1 = lifecycleOwner) {
         val observer = LifecycleEventObserver { _, event ->
+            println(event)
             lifecycle = event
         }
         lifecycleOwner.lifecycle.addObserver(observer)
@@ -71,10 +83,16 @@ fun PlayerTrailer (movieName: String){
             when (lifecycle) {
                 Lifecycle.Event.ON_RESUME -> {
                     it.onPause()
-                    it.player?.pause()
+                    it.player?.seekTo(playbackPosition)
+                    it.player?.play()
                 }
 
-                Lifecycle.Event.ON_PAUSE -> {
+                Lifecycle.Event.ON_STOP -> {
+                        it.onResume()
+                        playbackPosition = it.player?.currentPosition!!
+                    }
+
+                Lifecycle.Event.ON_DESTROY -> {
                     it.onResume()
                 }
 
--- app/src/main/java/com/example/movieappmad24/models/MoviesViewModel.kt
+++ app/src/main/java/com/example/movieappmad24/models/MoviesViewModel.kt
@@ -2,19 +2,23 @@ package com.example.movieappmad24.models
 
 import androidx.compose.runtime.toMutableStateList
 import androidx.lifecycle.ViewModel
+import com.example.movieappmad24.composables.PlayerTrailer
 import com.example.movieappmad24.models.Movie
 import com.example.movieappmad24.models.getMovies
 
 // Inherit from ViewModel class
 class MoviesViewModel : ViewModel() {
     private val _movieList = getMovies().toMutableList()
+
     // get all movies and create a StateHolder from it, so it can be observed by UI
     val movieList: List<Movie> // expose previously created list but immutable
         get() = _movieList
-// rest of logic
-val watchList: List<Movie> // expose previously created list but immutable
-    get() = _movieList.filter { movie -> movie.isFavourite }
 
-fun toggleFavoriteMovie(movieId: String) = _movieList.find { it.id == movieId }?.let { movie ->
-    movie.isFavourite = !movie.isFavourite }
+    // rest of logic
+    val watchList: List<Movie> // expose previously created list but immutable
+        get() = _movieList.filter { movie -> movie.isFavourite }
+
+    fun toggleFavoriteMovie(movieId: String) = _movieList.find { it.id == movieId }?.let { movie ->
+        movie.isFavourite = !movie.isFavourite
+    }
 }
